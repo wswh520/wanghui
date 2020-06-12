abab0cf1b33159c7d4ccb1d6241de66f8da17d92
wanghui <1446218560@qq.com>
2020-06-12 17:29:18 +0800

    版本更新

--- springboot-dubbo-client/src/main/java/com/example/dubbo/controller/ClientController.java
+++ springboot-dubbo-client/src/main/java/com/example/dubbo/controller/ClientController.java
@@ -1,17 +1,18 @@
 package com.example.dubbo.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.example.dubbo.service.ApiService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 
-//    @Reference(version = "1.0.0")
-//    private DubboServerService dubboServerService;
+    @Reference(version = "1.0.0")
+    private ApiService apiService;
 
     @RequestMapping("client")
     public String client(String parm){
-        return parm;
+        return apiService.getParm(parm);
     }
 }
