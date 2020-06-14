96e208ed72ff2f56a749981378ff8044d4f1cb45
wanghui <1446218560@qq.com>
2020-06-14 11:33:34 +0800

    版本更新

--- springboot-dubbo-server/src/main/java/com/example/dubbo/service/impl/DubboServerServiceImpl.java
+++ springboot-dubbo-server/src/main/java/com/example/dubbo/service/impl/DubboServerServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.dubbo.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.example.dubbo.service.ApiService;
 
-@Service
+@Service(version = "1.0.0")
 public class DubboServerServiceImpl implements ApiService {
 
     @Override
